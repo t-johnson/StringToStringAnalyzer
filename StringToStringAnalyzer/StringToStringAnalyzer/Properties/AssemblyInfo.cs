0ffe1760487ee9ecf3b43afb32698e79f55c8730
Tim Johnson <the.tim.johnson@gmail.com>
2016-12-22 21:18:26 -0500

    Conversion to VS2017 (RC)

--- StringToStringAnalyzer/StringToStringAnalyzer/Properties/AssemblyInfo.cs
+++ StringToStringAnalyzer/StringToStringAnalyzer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StringToStringAnalyzer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("String.ToString() Analyzer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StringToStringAnalyzer")]
-[assembly: AssemblyCopyright("Copyright © Tim Johnson 2016")]
+[assembly: AssemblyCopyright("Copyright © Tim Johnson 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
